68b30ba6629e62041a1cd4ac2cfcea2e02eb9177
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-11-14 22:09:42 +0100

    Updated Life UI and GameOver UI with UnityEvents

--- 3DP2_SanchezSergio_RigatCarles/Assets/Scripts/Others/PlayerStats.cs
+++ 3DP2_SanchezSergio_RigatCarles/Assets/Scripts/Others/PlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -8,24 +9,36 @@ public class PlayerStats : MonoBehaviour
     public float currentHealth;
     public float timeToRegen;
     public float regenCounter;
-    [SerializeField] GameObject gameOverHud;
+    [SerializeField] TitleScreen gameOverHud;
+    [SerializeField] UnityEvent<float, float> healthChange;
 
     private void Awake()
     {
         currentHealth = maxHealth;
     }
 
+    private void Start()
+    {
+        updateHealthUI();
+    }
+
     public void takeDamage(float damage)
     {
-        Debug.Log("Taken "+damage+" damage");
         currentHealth -= damage;
         if (currentHealth <= 0.0f)
         {
             currentHealth = 0;
             die();
         }
+        updateHealthUI();
         regenCounter = 0;
     }
+
+    void updateHealthUI()
+    {
+        healthChange.Invoke(maxHealth, currentHealth);
+    }
+
     private void Update()
     {
         regenCounter++;
@@ -42,13 +55,12 @@ public class PlayerStats : MonoBehaviour
     {
         if (collider.gameObject.tag == "deadzone")
         {
-            takeDamage(9999999999999);
+            takeDamage(float.MaxValue);
         }
     }
 
     private void die()
     {
-        gameOverHud.SetActive(true);
-        
+        gameOverHud.gameOver();
     }
 }
